a63adad7657178fef25bf21f39a333f7e5256934
Sunlyst <sunlyst.98@gmail.com>
2020-02-13 11:55:34 +0100

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms'
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  ideaForm : FormGroup
+
+  constructor(private fb: FormBuilder){
+
+    this.ideaForm = this.fb.group({
+      username: '',
+      description: ''
+    })
+  }
+
+  newIdea(){
+    console.log(this.ideaForm.value);
+  }
 }
