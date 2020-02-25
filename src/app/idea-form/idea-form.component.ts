c434428b9dec3a011e021ad0be27d2e257fb5469
Sunlyst <sunlyst.98@gmail.com>
2020-02-25 10:22:04 +0100

    1 file modified

--- src/app/idea-form/idea-form.component.ts
+++ src/app/idea-form/idea-form.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms'
+import { IdeaService, Idea } from '../idea.service';
 
 @Component({
   selector: 'app-idea-form',
   templateUrl: './idea-form.component.html',
   styleUrls: ['./idea-form.component.css']
 })
-export class IdeaFormComponent implements OnInit {
+export class IdeaFormComponent{
 
-  constructor() { }
+  ideaForm : FormGroup;
 
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private ideaService: IdeaService
+    ){
+
+    this.ideaForm = this.fb.group({
+      username: '',
+      description: ''
+    })
   }
 
+  newIdea(){
+    if(this.ideaForm.value.username != '' && this.ideaForm.value.description != '')
+    {  
+      this.ideaService.addIdea(this.ideaForm.value.username, this.ideaForm.value.description)
+    }
+  }
 }
