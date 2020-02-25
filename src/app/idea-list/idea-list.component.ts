3c8ba87bb47c3a3bd21eef43a2f3d86eabd5f505
Sunlyst <sunlyst.98@gmail.com>
2020-02-25 11:38:31 +0100

    3 files modified

--- src/app/idea-list/idea-list.component.ts
+++ src/app/idea-list/idea-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms'
 import { IdeaService, Idea } from '../idea.service';
 
 @Component({
@@ -8,12 +9,25 @@ import { IdeaService, Idea } from '../idea.service';
 })
 export class IdeaListComponent implements OnInit {
 
-  private ideas : idea[];
+  private ideas : Idea[];
+  ideaForm : FormGroup;
 
-  constructor(private ideaService: IdeaService) { 
+  constructor(
+    private ideaService: IdeaService,
+    private fb: FormBuilder,
+    ){
 
-    
+    this.ideaForm = this.fb.group({
+      username: '',
+      description: ''
+    })
+  }
 
+  newIdea(){
+    if(this.ideaForm.value.username != '' && this.ideaForm.value.description != '')
+    {  
+      this.ideaService.addIdea(this.ideaForm.value.username, this.ideaForm.value.description)
+    }
   }
 
 
