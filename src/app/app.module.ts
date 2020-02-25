564bf990a395c36d464e61a572aa4bb9a52c7b97
Sunlyst <sunlyst.98@gmail.com>
2020-02-25 10:16:31 +0100

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import {ReactiveFormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { IdeaService } from './idea.service';
 import { IdeaListComponent } from './idea-list/idea-list.component';
+import { IdeaFormComponent } from './idea-form/idea-form.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, IdeaListComponent ],
+  declarations: [ AppComponent, IdeaListComponent, IdeaFormComponent ],
   bootstrap:    [ AppComponent ],
   providers: [IdeaService]
 })
